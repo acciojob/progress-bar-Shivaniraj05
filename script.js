0e5fc62d43fb3dbd6aa3f058af382408a1f29040
Shivaniraj05 <86128017+Shivaniraj05@users.noreply.github.com>
2023-03-28 22:13:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+  if (currentStep === circles.length) {
+    buttons[1].disabled = true;
+  } else if (currentStep === 1) {
+    buttons[0].disabled = true;
+  } else {
+    buttons.forEach((button) => (button.disabled = false));
+  }
+};
